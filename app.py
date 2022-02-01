b33d988e4f99fbc1d7e15f2dde4748800917f4d8
AlexNik84 <42363137+AlexNik84@users.noreply.github.com>
2022-01-31 21:25:32 -0500

    Add files via upload
    Completed python code to retrieve bottom 100 movies from IMDB, sort them by director's name and write to json file.

---
+++ app.py
@@ -0,0 +1,28 @@
+from imdb import IMDb
+import json
+
+ia = IMDb()
+bot100 = ia.get_bottom100_movies()
+
+movies = []
+titles_list = []
+directors_list = []
+
+for i in range(0, 99):
+    movie = ia.get_movie(bot100[i].movieID)
+    titles_list.append(movie['title'])
+    for director in movie['directors']:
+        directors_list.append(director['name'])
+        break
+    movies.append({'Title': titles_list[i], "Directors": directors_list[i]})
+    print(movies)
+
+sorted_movies = sorted(movies, key = lambda d: d['Directors'])
+
+def write_to_json(d):
+    with open('C:/path_to_file/movies.json', 'w') as file: #Used C:/ since my current system is Windows
+        json.dump(d, file, indent=4)
+        file.truncate()
+        file.close()
+
+write_to_json(sorted_movies)
